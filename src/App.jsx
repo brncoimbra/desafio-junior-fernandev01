c38d734b36ae974dda9005c9b9f5d5d792f05813
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-05-22 14:27:38 -0300

    final version

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,62 @@ import "./App.css";
 
 function App() {
   const [list, setList] = useState([]);
+  const [redo, setRedo] = useState([]);
 
   const handleClick = (event) => {
     const newDot = {
       clientX: event.clientX,
       clientY: event.clientY,
     };
-
+    console.log(list);
     setList((prev) => [...prev, newDot]);
+  };
+
+  const handleUndo = (event) => {
+    event.stopPropagation();
+
+    if (list.length === 0) {
+      return;
+    }
+
+    const lastItem = list[list.length - 1];
+    setRedo((prev) => [...prev, lastItem]);
+
+    setList((prev) => {
+      const newArr = [...prev].slice(0, -1);
+
+      return newArr;
+    });
+  };
+
+  const handleRedo = (event) => {
+    event.stopPropagation();
+
+    if (redo.length === 0) {
+      return;
+    }
+
+    const recoveredDot = redo[redo.length - 1];
+
+    setRedo((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    });
+    setList((prev) => [...prev, recoveredDot]);
   };
 
   return (
     <>
       <div className='box' onClick={handleClick}>
-        {JSON.stringify(list)}
-        <span className='dot' />
+        <button onClick={handleUndo}>Desfazer</button>
+        <button onClick={handleRedo}>Refazer</button>
+        {list.map((item, index) => (
+          <span
+            key={index}
+            className='dot'
+            style={{ left: item.clientX, top: item.clientY }}
+          />
+        ))}
       </div>
     </>
   );
